add6a2b02c269c1f5ecadfca2b7642c1e74fcbb3
yanFajR <imryanfajri@gmail.com>
2025-11-22 16:21:09 +0700

    halaman single page

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<x-layout :title="$title">
+    <article class="py-8 max-w-screen-md">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">
+            {{ $post['title'] }}
+        </h2>
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post['author'] }}</a> | 16 November 2025
+        </div>
+        <p class="my-4 font-light">
+            {{ $post['body'] }}
+        </p>
+        <a href="/posts " class="font-medium text-blue-500 hover:underline">&laquo Back to all posts
+        </a>
+    </article>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,8 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     $posts = [
         [
+            'id' => 1,
+            'slug' => 'judul-artikel',
             'title' => 'Judul Artikel',
             'author' => 'Ryan Fajri',
             'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
@@ -17,6 +20,8 @@ Route::get('/posts', function () {
             ratione? Aliquam, corrupti labore!'
         ],
         [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'author' => 'Ryan Fajri',
             'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
@@ -27,6 +32,41 @@ Route::get('/posts', function () {
     ];
 
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel',
+            'title' => 'Judul Artikel',
+            'author' => 'Ryan Fajri',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. In
+            expedita quaerat asperiores odit obcaecati distinctio sit quis
+            impedit id, dicta perspiciatis velit porro debitis ex consectetur
+            ratione? Aliquam, corrupti labore!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Ryan Fajri',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
+            eligendi aliquam nobis eius necessitatibus perferendis soluta veniam
+            exercitationem possimus, doloremque nisi adipisci culpa fugit,
+            aliquid voluptates. Voluptate voluptas ipsum sed!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    if (!$post) {
+        abort(404);
+    }
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
